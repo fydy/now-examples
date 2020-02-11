2b39ac878b1bb30cb8e33644d6a08e4230b4f789
E-Law <barberw@mail.lcc.edu>
2020-02-11 10:56:12 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,7 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
-Tracy\Debugger::enable(Tracy\Debugger::DEVELOPMENT);
-
-throw new RuntimeException('Hello Tracy!');
+/** WordPress view bootstrapper */
+define('WP_USE_THEMES', true);
+require __DIR__ . '/wp/wp-blog-header.php';
+require_once __DIR__ . '/web/wp/wp-blog-header.php';
